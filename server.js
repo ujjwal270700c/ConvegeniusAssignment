fef839bfc79b0ccb3facd6948fc9a8fad1ab79d7
ujjwal270700c <ujjwal270700c@gmail.com>
2021-10-14 18:41:30 +0530

    telegram bot init

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express')
 const app=express();
 require('dotenv').config()
 const port =process.env.PORT;
+const axios=require('axios')
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
@@ -10,6 +11,40 @@ app.get('/',(req,res)=>{
     res.send('Hello')
 })
 
-app.listen(port,()=>{
+
+const { Token, SERVER_URL } = process.env
+const TELEGRAM_API = `https://api.telegram.org/bot${Token}`
+const URI = `/webhook/${Token}`
+const WEBHOOK_URL = SERVER_URL + URI
+
+
+
+const init = async () => {
+
+try {
+    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
+    console.log(res.data)
+} catch (error) {
+    console.log(error);
+}
+  
+}
+
+app.post(URI, async (req, res) => {
+    console.log(req.body)
+
+    // const chatId = req.body.message.chat.id
+    // const text = req.body.message.text
+
+    // await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    //     chat_id: chatId,
+    //     text: text
+    // })
+    return res.send()
+})
+
+
+app.listen(port,async()=>{
     console.log(`Server is running at port :${port}`);
+    await init()
 })
